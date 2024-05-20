67e475a5765abffeae8b01d6b226b2ddeeb66710
zolotareff <77545502+zolotareff@users.noreply.github.com>
2024-05-20 17:24:10 -0300

    .
    .

--- app.js
+++ app.js
@@ -1,85 +1,68 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database: "auto"
+    host:'sql10.freemysqlhosting.net',
+    user:'sql10701770',
+    password:'uh4Vi5Y7dZ',
+    database:'sql10701770'
 })
-    connection.connect()
-   
 
-//Select
-//module.exports = function select(d) {
-const selecionar = function selected(d) {    
-let sql = "select * from carro where id = ?"
-let id = d
-connection.query(sql, id, function(error, results, fields){
-   
-    if(error) throw error 
-    let carros = results
-    for(let i = 0; carros.length > i;i++){
-        console.log(carros[i].id + ":" + carros[i].marca)
-        return
-    }   
-    
-})
-     connection.end()
+connection.connect()
+
+//inserir dados - insert into nometabela
+function inserir(lg,sn,mail) {
+    let sql = "insert into users set ?"
+    let dados = {login:lg,senha:sn,email:mail}
+
+    connection.query(sql,dados, function(error,results,fields){
+        if(error) throw error
+        console.log("cadastro com sucesso!")
+    })
+}
+//selecionar dados por id - select nometabela
+function selecionaPorId(id){
+    let sql ="select * from users where id = ?"
+    connection.query(sql,id, function(error,results,fields){
+                 if(error) throw error
+                 console.log("selecionado: " + results[0].id + ":" + results[0].login +
+                                                                          ":" + results[0].email)
+        
+ 
+
+        
+    })
 }
 
-//Select Marca
-    const selecionarMarca = function selectedNome(d) {    
-        let sql = "select * from carro where marca = ?"
-        let marca = d
-        connection.query(sql, marca, function(error, results, fields){
-           
-            if(error) throw error 
-            let carros = results
-            for(let i = 0; carros.length > i;i++){
-                console.log(carros[i].marca)
-                return
-            }   
-            
-        })
-             connection.end()
+
+//selecionar dados por id - select nometabela
+function selecionaTudo(){
+    let sql ="select * from users "
+    connection.query(sql, function(error,results,fields){
+
+        for(let i=0;i<results.length;i++) {
+                 if(error) throw error
+                 console.log("selecionado: " + results[i].id + ":" + results[i].login +
+                                                                          ":" + results[i].email)
         }
+        
+ 
 
-//Insert
-const inserir = function insert(marca,ano){
-let sql = "insert into carro set ?"
-let dados = {marca:marca,ano:ano}
-connection.query(sql, dados, function(error, results, fields){
-    if(error) throw error
-    console.log("Salvo" + results.insertId)
-})
-    connection.end()
+        
+    })
 }
 
-//Atualizer
-const atualizar = function update(cod,marca,ano){
-let sql = "update carro set ? where id = ?"
-let dados = {id:cod,marca:marca,ano:ano}
-let id = dados.id
-connection.query(sql,[dados,id], function(error,results,fields){
-    if(error) throw error
-    console.log("Atualizado: " + results.affectedRows)
-})
-    connection.end()
+selecionaTudo()
+
+
+//Apagar dados - delete from nometabela
+function apagar(id){
+    let sql = "delete from users where id = ?"
+    connection.query(sql,id, function(error,results,fields){
+        if(error) throw error
+        console.log("Apagado com sucesso! " + id)
+
+    })
 }
 
-//Apagar
-const apagar = function del(cod){
-let sql = "delete from carro where id = ?"
-let id = cod
-connection.query(sql,id, function(error,results,fields){
-    if(error) throw error
-    console.log("Apadado com sucesso")
-})
-    connection.end()
-}
 
-exports.selecionar = selecionar
-exports.selecionarMarca = selecionarMarca
-exports.inserir = inserir
-exports.atualizar = atualizar
-exports.apagar = apagar
+connection.end()
